d75a987fa0d765a9495b3a32054ea8eef26b81b1
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-13 10:58:29 +0100

    feact(automapper & controller): Implement complex type auto mapper adding an entity controller.

---
+++ AutoMapperComplexMappingDemo/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using AutoMapperComplexMappingDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMapperComplexMappingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ECommerceDBContext _context;
+        private readonly IMapper _mapper;
+
+        public OrdersController(ECommerceDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+    }
+}
---
+++ AutoMapperComplexMappingDemo/MappingProfiles/OrderMappingProfile.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using AutoMapperComplexMappingDemo.DTOs;
+using AutoMapperComplexMappingDemo.Models;
+
+namespace AutoMapperComplexMappingDemo.MappingProfiles
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile() {
+
+            CreateMap<Order, OrderDTO>()
+                .ForMember(
+                dest => dest.OrderId, 
+                opt => opt.MapFrom<int>(src => src.Id))
+                .ForMember(
+                dest => dest.OrderDate, 
+                opt => opt.MapFrom<string>(src => src.OrderDate.ToString("yyyy-MM-dd")))
+                .ForMember(
+                dest => dest.CustomerName, 
+                opt => opt.MapFrom<string>(src => $"{src.Customer.FirstName} {src.Customer.LastName}"))
+                .ForMember(
+                dest => dest.CustomerEmail, 
+                opt => opt.MapFrom<string>(src => src.Customer.Email))
+                .ForMember(
+                dest => dest.CustomerPhoneNumber, 
+                opt => opt.MapFrom<string>(src => src.Customer.PhoneNumber))
+                .ForMember(
+                dest => dest.Address, 
+                opt => opt.MapFrom<Address>(src => src.Customer.Address))
+                .ForMember(
+                dest => dest.Items, 
+                opt => opt.MapFrom<ICollection<OrderItem>>(src => src.OrderItems));
+
+            // Mapping configuration for customer addresses
+            // Maps the Address entity to AddressDTO for data transfer
+            CreateMap<Address, AddressDTO>(); //Propery Mapping is not required as both contains the same property names
+
+            // Mapping configuration for order items
+            // Maps the OrderItem entity to OrderItemDTO for data transfer
+            CreateMap<OrderItem, OrderItemDTO>()
+                // Map the Product.Name property to OrderItemDTO.ProductName
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+                // Map the Product.Price property to OrderItemDTO.ProductPrice
+                .ForMember(dest => dest.ProductPrice, opt => opt.MapFrom(src => src.Product.Price))
+                // Calculate and map the total price (Product.Price * Quantity) to OrderItemDTO.TotalPrice
+                .ForMember(dest => dest.TotalPrice, opt => opt.MapFrom(src => src.Product.Price * src.Quantity));
+
+            //Property mapping is not required since they contain same property.
+            CreateMap<OrderItemCreateDTO, OrderItem>();
+
+        }
+
+    }
+}
